e52a796fc4403e970b79783c7256f89b20ba9c5c
Kiana Omoerah <kiana.omoerah@gmail.com>
2022-10-21 17:49:37 -0400

    function to filter cities array for city and states matching user input

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-  const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
   
 const cities = [];
 
 fetch(endpoint)
 .then( data => data.json())
 .then( data => cities.push(...data))
+
+const findMatches = (wordToMatch, cities) => {
+    return cities.filter(place => {
+        // here we need to figure out if the city or state matches what was searched
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.city.match(regex) || place.state.match(regex)
+    
+    })
+
+}
