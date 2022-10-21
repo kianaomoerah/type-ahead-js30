cf582402faf36f9d2972197b598928189c937ad4
Kiana Omoerah <kiana.omoerah@gmail.com>
2022-10-21 17:54:09 -0400

    set basic display match function, query selected search and suggestion class, added change and keep up event listener to trigger display match function

--- script.js
+++ script.js
@@ -11,7 +11,15 @@ const findMatches = (wordToMatch, cities) => {
         // here we need to figure out if the city or state matches what was searched
     const regex = new RegExp(wordToMatch, 'gi');
     return place.city.match(regex) || place.state.match(regex)
-    
-    })
-
+    });
 }
+
+function displayMatches() {
+    console.log(this.value)
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
