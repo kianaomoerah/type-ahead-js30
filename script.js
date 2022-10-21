fbfc6eb1e07b216a564bd7880af77ea2b962958b
Kiana Omoerah <kiana.omoerah@gmail.com>
2022-10-21 18:06:40 -0400

    added function to add commas to population results

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ const findMatches = (wordToMatch, cities) => {
     });
 }
 
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
 function displayMatches() {
     const matchArray = findMatches(this.value, cities);
     const html = matchArray.map(place => {
@@ -24,7 +28,7 @@ function displayMatches() {
         return `
             <li>
             <span class="name">${cityName}, ${stateName}</span>
-            <span class="population">${place.population}</span>
+            <span class="population">${numberWithCommas(place.population)}</span>
             </li>        
         `;
     }).join('');
